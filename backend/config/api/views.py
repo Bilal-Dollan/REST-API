ca2a929c312fc7f29525509dad39a434bc9a1e78
Bilal Dollan <bilaldollan199@gmail.com>
2022-05-20 13:20:15 +0300

    second commit

---
+++ backend/config/api/views.py
@@ -0,0 +1,6 @@
+from django.http import JsonResponse
+
+
+def api_home(request):
+    return JsonResponse({'message': 'Helloo'})
+
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,6 @@
 import requests
 
+endpoint = 'http://127.0.0.1:8000/api'
 
-endpoint = 'http://127.0.0.1:8000/'
-
-get_response = requests.get(endpoint, json = {'hello': 'HIIII'})
-print(get_response.status_code)
+get_response = requests.get(endpoint, json={'hello': 'HIIII'})
+print(get_response.json())
