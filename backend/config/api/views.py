92ca6f4e8a30e0eb1d6de4983a9ad7c5f97d4000
Bilal-Dollan <bilaldollan199@gmail.com>
2022-05-21 16:14:06 +0300

    4th commit

--- backend/config/api/views.py
+++ backend/config/api/views.py
@@ -1,15 +1,13 @@
 from django.http import JsonResponse
 import json
+from product.models import Product
+from django.forms.models import model_to_dict
 
 
 def api_home(request):
-    body = request.body
-    print(body)
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body)
-    finally:
-        pass
-    print(data)
+    if model_data:
+        data = model_to_dict(model_data, fields=['id', 'title'])
 
     return JsonResponse(data)
